e1ba131b46a9748dd955d30d20c33028ad205153
pulkit gupta <pulvar234@gmail.com>
2025-11-24 15:16:22 +0530

    Refactor NoConversationPlaceholder component
    Refactor NoConversationPlaceholder to use arrow function and add styling.

--- frontend/src/components/NoConversationPlaceholder.jsx
+++ frontend/src/components/NoConversationPlaceholder.jsx
@@ -1,4 +1,7 @@
-function NoConversationPlaceholder() {
-    return <div>NoConversationPlaceholder</div>
-}
+const NoConversationPlaceholder = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-full text-center p-6">
+    </div>
+  );
+};
 export default NoConversationPlaceholder;
